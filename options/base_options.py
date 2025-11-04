6ff37979db2048a0d23de5460901e0cbe5370b1f
tabbyblack <tb789@cam.ac.uk>
2025-11-04 22:07:05 +0000

    cleaning up

--- options/base_options.py
+++ options/base_options.py
@@ -24,7 +24,7 @@ class BaseOptions:
         parser.add_argument("--name", type=str, default="experiment_name", help="name of the experiment. It decides where to store samples and models")
         parser.add_argument("--checkpoints_dir", type=str, default="./checkpoints", help="models are saved here")
         # model parameters
-        parser.add_argument("--model", type=str, default="cycle_gan", help="chooses which model to use. [cycle_gan | pix2pix | test | colorization]")
+        parser.add_argument("--model", type=str, default="cycle_gan", help="chooses which model to use. [cycle_gan | test | colorization]")
         parser.add_argument("--input_nc", type=int, default=3, help="# of input image channels: 3 for RGB and 1 for grayscale")
         parser.add_argument("--output_nc", type=int, default=3, help="# of output image channels: 3 for RGB and 1 for grayscale")
         parser.add_argument("--ngf", type=int, default=64, help="# of gen filters in the last conv layer")
@@ -55,7 +55,7 @@ class BaseOptions:
         parser.add_argument("--suffix", default="", type=str, help="customized suffix: opt.name = opt.name + suffix: e.g., {model}_{netG}_size{load_size}")
         # wandb parameters
         parser.add_argument("--use_wandb", action="store_true", help="if specified, then init wandb logging")
-        parser.add_argument("--wandb_project_name", type=str, default="CycleGAN-and-pix2pix", help="specify wandb project name")
+        parser.add_argument("--wandb_project_name", type=str, default="CycleGAN", help="specify wandb project name")
         self.initialized = True
         return parser
 
