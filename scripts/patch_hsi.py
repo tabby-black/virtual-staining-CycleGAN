a4ecb703f6f068fd9200bbce5ab634837d56fbcd
tabbyblack <tb789@cam.ac.uk>
2025-12-11 12:12:05 +0000

    rgb patching

--- scripts/patch_hsi.py
+++ scripts/patch_hsi.py
@@ -26,9 +26,9 @@ def patch_hsi(hdr_path):
     }
 
     # save the 4 patches with the correct metadata
-    for key, cube in patches.items():
+    for key, patch in patches.items():
         out_hdr = os.path.join(dirname, f"{basename}_{key}.hdr")
-        envi.save_image(out_hdr, cube.astype(np.float32), dtype=np.float32, force=True, interleave='bsq')
+        envi.save_image(out_hdr, patch.astype(np.float32), dtype=np.float32, force=True, interleave='bsq')
 
     # remove the original full image
     os.remove(hdr_path)
@@ -49,5 +49,43 @@ for hdr in hdr_train_files:
 for hdr in hdr_test_files:
     patch_hsi(hdr)
 
+
+
 # patch rgb images
-rgb = 
+def patch_rgb(image_path):
+    rgb = Image.open(image_path)
+    W, H = rgb.size
+
+    dirname = os.path.dirname(image_path)
+    basename = os.path.splitext(os.path.basename(image_path))[0]
+
+    # compute 4 non-overlapping patches
+    patches = {
+        "top_left": rgb.crop((0,    0,  W//2,   H//2)),
+        "top_right": rgb.crop((W//2, 0,  W,  H//2)),
+        "bottom_left": rgb.crop((0, H//2,   W//2,   H)),
+        "bottom_right": rgb.crop((W//2, H//2,   W,  H))
+    }
+
+    # save the 4 patches
+    for key, patch in patches.items():
+        out_png = os.path.join(dirname, f"{basename}_{key}.png")
+        patch.save(out_png)
+
+    # remove the original full image
+    os.remove(image_path)
+
+
+# call this function on all images in trainB and testB - using glob
+train_directory = "/datasets/trainB"
+test_directory = "/datasets/testB"
+
+png_train_files = sorted(glob.glob(os.path.join(train_directory, "*.png")))
+
+png_test_files = sorted(glob.glob(os.path.join(test_directory, "*.png")))
+
+for png in png_train_files:
+    patch_rgb(png)
+
+for png in png_test_files:
+    patch_rgb(png)
