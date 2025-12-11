764a46d3e79faf5d647d51dcece27c97da36070d
tabbyblack <tb789@cam.ac.uk>
2025-12-11 08:29:30 +0000

    patching

--- scripts/patch_hsi.py
+++ scripts/patch_hsi.py
@@ -1,1 +1,53 @@
 # data preprocessing step 3
+
+from spectral import open_image, envi
+import numpy as np
+import os
+import glob
+
+from PIL import Image
+from itertools import product
+
+# patch hyperspectral images
+def patch_hsi(hdr_path):
+    # load hyperspectral cube
+    img = open_image(hdr_path).load
+    H, W, B = img.shape
+
+    dirname = os.path.dirname(hdr_path)
+    basename = os.path.splitext(os.path.basename(hdr_path))[0]
+
+    # compute 4 non-overlapping patches
+    patches = {
+        "top_left": img[0:H//2,   0:W//2, :],
+        "top_right": img[0:H//2,   W//2:W, :],
+        "bottom_left": img[H//2:H,   0:W//2, :],
+        "bottom_right": img[H//2:H,   W//2:W, :]
+    }
+
+    # save the 4 patches with the correct metadata
+    for key, cube in patches.items():
+        out_hdr = os.path.join(dirname, f"{basename}_{key}.hdr")
+        envi.save_image(out_hdr, cube.astype(np.float32), dtype=np.float32, force=True, interleave='bsq')
+
+    # remove the original full image
+    os.remove(hdr_path)
+    raw_path = hdr_path.replace(".hdr", "")
+    os.remove(raw_path)
+
+# call this function on all images in trainA and testA - using glob
+train_directory = "/datasets/trainA"
+test_directory = "/datasets/testA"
+
+hdr_train_files = sorted(glob.glob(os.path.join(train_directory, "*.hdr")))
+
+hdr_test_files = sorted(glob.glob(os.path.join(test_directory, "*.hdr")))
+
+for hdr in hdr_train_files:
+    patch_hsi(hdr)
+
+for hdr in hdr_test_files:
+    patch_hsi(hdr)
+
+# patch rgb images
+rgb = 
