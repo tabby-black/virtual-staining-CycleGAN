564c885b088ede384dbfeebe705e66f6f5fda0c2
tabbyblack <tb789@cam.ac.uk>
2025-12-16 21:46:47 +0000

    regisration script made

---
+++ scripts/image_registration.py
@@ -0,0 +1,5 @@
+# perform image registration to get rough alignment of hyperspectral and rgb images for evaluation purposes
+
+# image registration (using Discrete Fourier Transform)
+import imreg_dft as ird
+
