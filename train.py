74bd51931a9abacb8a46208a38e53008b3d88be8
tabbyblack <tb789@cam.ac.uk>
2025-12-23 12:59:44 +0000

    visualisation edits

--- train.py
+++ train.py
@@ -56,7 +56,7 @@ if __name__ == "__main__":
             epoch_iter += opt.batch_size
             model.set_input(data)  # unpack data from dataset and apply preprocessing
             model.optimize_parameters()  # calculate loss functions, get gradients, update network weights
-
+            
             if total_iters % opt.display_freq == 0:  # display images on visdom and save images to a HTML file
                 save_result = total_iters % opt.update_html_freq == 0
                 model.compute_visuals()
